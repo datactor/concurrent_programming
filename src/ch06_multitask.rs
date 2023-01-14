5ba95ec8a50172219895cf23eb1810550bfbde10
jaemin <datactress@gmail.com>
2023-01-14 20:15:18 +0900

    feat: ch06

--- src/ch06_multitask.rs
+++ src/ch06_multitask.rs
@@ -66,6 +66,95 @@
 // 하고 있기 때문이다. context switch를 전혀 수행하지 않는 OS도 존재하는데 이런 OS는 싱글 태스크 OS라 부르며
 // context switch를 수행하는 여러 OS 프로세스를 동시에 작동시키는 것이 가능한 OS는 멀티 태스크 OS라 부른다.
 // 윈도우, 리눅스, BSD 계열 OS 등 주류 OS는 multi-task OS이고, 싱글 태스크 OS는 윈도우의 전신인 MS-DOS가 유명하다.
+//
+// multi-task가 가능한 실행 환경이란 여러 프로세스를 실행할 수 있는 환경으로 명백하지만, 이런 실행 환경을 만드는 것은
+// 잘 생각해보면 사실 어렵다. 예를 들어 CPU가 4개인 환경에서 최대 4개까지의 프로세스를 동시에 실행할 수 있는 4코어 OS가
+// 있다고 하자. 이 OS는 여러 프로세서를 실행할 수 있지만 실질적으로 싱글태스크 OS와 다르지 않다. 또한 3개, 4개, 5개의
+// 프로세스를 충분히 생성할 수 있지만, last in, first out과 같이 가장 마지막에 생성한 프로세스가 종료될 때까지
+// 이전에 생성된 프로세스가 실행되지 않는 경우에도 실질적으로는 싱글 태스크 OS와 다르지 않다(동시에 실행하는 프로세스의
+// 개수가 하나라면 여러개 실행하더라도 싱글태스크와 다름 없다). 그래서 이 책에서는 multi-task 실행 환경을 다음과 같이
+// 정의한다. 실행 환경이란? OS, 의사 머신, 언어 처리 시스템을 총칭한다.
+// - 정의 multi-task 실행 환경: 어떤 실행 환경이 multi task 가능하다. <-> 임의의 시점에서 새로운 프로세스를
+//   생성할 수 있고, 계산 도중 상태에 있는 프로세스가 공평하게 실행된다.
+//
+// 공평성은 다음 두 가지로 정의한다.
+// - 정의 weak 공평성: 어떤 실행 환경이 약한 공평성을 만족한다. <-> 어떤 프로세스가 특정 시각 이후 실행 가능한
+//   대기 상태가 되었을 때 그 프로세스가 실행된다.(메모리 엑세스 순서에 대한 보장이 없는 메모리 모델. 메모리
+//   엑세스가 잘못된 순서로 발생하거나 특정 메모리 엑세스가 완전히 손실될 수 있음을 의미함)
+//
+// - 정의 strong 공평성: 어떤 실행 환경이 강한 공평성을 만족한다. <-> 어떤 프로세스가 특정 시각 이후 실행 가능한
+//   대기 상태와 실행 불가능한 대기 상태의 전이를 무한히 반복할 때 최종적으로 그 프로세스는 실행된다.
+//   (메모리 엑세스 순서에 대한 보장이 있는 메모리 모델. 메모리 엑세스가 항상 요청된 순서대로 발생하는 것으로 손실되지
+//   않음. std::sync::atomic 및 std::sync::atomic_weak 모듈을 통해 지원됨)
+// 이들 정의는 선형 시제 논리(Linear Temporal Logic; LTL 또는 Propositional Temproal Logic; PTL이라고도 부름)
+// 로 정식화된다. 선형 시제 논리는 논리학에서 선형 이산 시간에 대한 여러 양상을 갖춘 시제 논리중 하나다. 예를 들어보자
+// 약한 공평성: 어떤 시각 이후 특정 시점에 놀러 가고 싶다고 생각하는 사람이 있을 때 그 사람을 데리고 갈 수 있는 환경.
+// 강한 공평성: 어떤 시각 이후 특정 시점에 놀러 가고 싶지만 다른 시점에는 집안에 틀어박혀 있고 싶어 하는 귀찮은 사람
+//            이라도 언젠가는 밖으로 데리고 놀러 갈 수 있는 환경이다.
+// 현실적인 시스템에서는 약한 공평성 구현이 필수이며 강한 공평성을 실현하는 것은 쉽지 않다.
+// 한편 현실의 구현에서는 latency와 CPU 시간의 배분이라는 관점도 포함해서 공평성에 관해 논의되고 있다. 예를 들어
+// 리눅스 커널 버전 2.6.33 이후에서는 완전히 공평한 스케줄링(Completely Fair Scheduling; CFS)라는 프로세스
+// 실행 방법을 채택하여 각 프로세스가 공평하게 CPU 시간을 소비 가능하게 변경되었다. 리눅스는 스케줄링 방식을 몇 가지
+// 선택할 수 있으며, IO의 deadline 근방의 process를 우선하는 스케줄러 등도 선택할 수 있다. 이렇게 현실적인 시스템에서
+// 공평성을 논할 때는 실행 가능성은 물론 리소스 소비의 관점도 고려해야 한다.
+
+
+// 6.1.2 협조적/비협조적 multi-task
+// context switch란 CPU상에서 프로세스의 전환을 수행하는 것이다. context switch를 수행하는 전략으로 협조적, 비협조적
+// 수행 방법이 있다. 협조적 전략은 프로세스 자신이 자발적으로 context switch 전환을 수행하는 방법이고, 비협조적 전략은
+// allocation 등 외부적인 강제력에 의해 context switch를 수행하는 방법이다. 협조적으로 context switch를 수행하는
+// multi-task를 협조적 멀티태스크, 비협조적으로 context switch를 수행하는 multi-task를 비협조적 multi-task라 부른다
+// 협조적 multi-task: 프로세스 자신이 능동적으로 context switch 전환을 수행하는 multi-task
+// 비협조적 multi-task: allocation 등 외부적인 강제력에 의해 수동적으로 context switch 전환을 수행하는 multi-task
+// - 정의 협조적 multi-task: 각각의 프로세스가 자발적으로 context switch를 수행하는 multi-task 방식
+// - 정의 비협조적 multi-task: 프로세스와 협조 없이 외부적인 작동에 따라 context switch를 수행하는 multi-task 방식
+//
+// cooperative multi-tasking은 비선점적 멀티태스킹(non-preemptive multi-tasking)
+// non-cooperative multi-tasking은 선점적 멀티태스킹(preemptive multi-tasking)이라 부르기도 한다.
+// 선점하다(preempt)라는 단어는 TV의 채널을 바꾸는 등의 의미를 가지고 있어서 이런 이름을 사용하게 된 것이라 생각된다.
+// 이와 관련된 용어인 선점(preemption)은 다음과 같이 정의한다.
+// - 정의 선점(preemption): process와의 협조 없이 수행하는 context switching
+// process의 context switch 방법을 결정하기 위한 module, func, process를 scheduler라 부르며 간단히 다음과
+// 같이 정의한다.
+// - 정의 scheduler: context switch 전략을 결정하는 process, module, func 등
+// scheduler는 공평성을 고려해 다음에 실행할 프로세스를 결정하고, non-cooperative multi-tasking인 경우에는
+// 어떤 타이밍에 preemption을 수행할 것인지도 결정해야 한다. scheduler가 process의 실행 순서를 결정하는 것을
+// scheduling이라 부른다.
+//
+// cooperative multi-tasking의 장점과 단점
+// 협조적 multi-task의 장점:
+// 1) multi-task mechanism을 쉽게 구현할 수 있다는 것을 들 수 있다. 그렇기 때문에
+//    초기 multi-tasking OS 대부분이 이를 채용했다. 예를 들어 윈도우 3.1이나 classic mac용 OS에서는
+//    협조적 multi-tasking 방식이 채용되어 있다. 그리고 Rust나 Python의 async/await이라는 mechanism은
+//    협조적 multi-tasking방식의 일종으로 현재도 이용되고 있다.
+// 협조적 multi-task의 단점:
+// 1) process가 자발적으로 context switch를 수행해야 한다는 것이다. 예를 들어 어떤 process에 버그가 있어
+//    context switch를 수행하지 않고 무한 loop에 빠지거나 정지하게 되면 그 process는 계산 resource를 점유하게 된다.
+//    그렇기 때문에 앱 개발자는 협조적 multi-task를 인식하고 이런 버그가 없도록 구현해야 한다. 윈도우 3.1이나
+//    classic mac의 경우에는 앱이 깨지면 OS 전체가 깨지며 PC를 재기동해야 하는 일이 빈번하게 일어났다.
+//
+// Rust나 Python에서의 async/await을 이용한 구현도 같은 문제를 안고 있으며, 이 mechanism을 이용해 구현해도
+// 무한 loop가 되거나 처리를 정지시키는 함수(blocking 함수)를 호출하면 context switch가 일어나지 않고 실행속도가
+// 낮아지거나 최악의 경우 deadlock에 빠지게 된다. async/await 등의 mechanism을 사용할 때는 협조적 프로그래밍인 것을
+// 정확하게 인식하고 구현 및 실행해야 한다.
+//
+// non-cooperative multi-tasking의 장점과 단점
+// 비협조적 multi-task의 장점
+// 1) 비협조적 multi-tasking에서는 협조적 multi-tasking에서의 무한 loop, blocking 함수와 관련된 문제는 일어나지
+//    않는다. 이들을 실행하거나 호출 중이라 해도 scheduler에 의해 preemption된 다른 process가 실행되기 때문이다.
+//    (스케줄러가 process order를 강제하기 때문) 윈도우나 리눅스 등의 현대적인 OS에서는 비협조적 multi-tasking을
+//    적용하고 있으므로 앱의 crash가 OS의 crash로 연결되는 경우는 드물다. 비협조적 multi-tasking을 적용한 프로그래밍 언어
+//    처리 계열로는 Erlang, Go 등의 언어가 있다.
+// 비협조적 multi-task의 단점
+// 2) 처리 시스템 구현이 어렵다. 하지만 앱을 구현하는 사람 입장에서는 처리 시스템 구현의 어려움을 크게 신경쓰지 않을
+//    것이다. 또한 공평성을 확보하기 위해 빈번하게 context switch를 수행하기도 하므로 협조적 multi-task에 비해
+//    다소 overhead가 있다.
+
+
+// 6.2 cooperative green thread 구현
+// Rust를 사용해 AArc64상에서 작동하는 간단한 cooperative multi-tasking 구현을 해보자. 이번 구현은 userland
+// thread이며, userland의 software가 독자적으로 제공한 thread mechanism은 일반적으로 green thread라 부른다.
+// green thread는 OS의 스레드와 비교해 스레드 생성과 파기 비용을 줄일 수 있으므로
 fn func_() {
 
 }
